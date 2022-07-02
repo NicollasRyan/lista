537bd4eb2263ffb9d7d6b5300ccee2e4523298e2
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-07-02 10:21:34 -0300

    Organizing Project

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+
+
+function Home() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Meus Filme e séries favoritos</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
