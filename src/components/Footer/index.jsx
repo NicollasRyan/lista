f6c409281c25567dde4f926fde72332ca70be773
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-07-02 14:44:55 -0300

    added src/components/ folder to git

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export function Footer() {
+    return (
+        <div>
+            <footer>site feita por Nicollas ryan</footer>
+        </div>
+    )
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export function Header() {
+    return (
+        <div>
+            <header>
+                <h1>Meus Filme e séries favoritos</h1>
+            </header>
+        </div>
+    )
+}
