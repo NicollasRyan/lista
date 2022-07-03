0fa78973be8911fa7066fc02b647f13dadb82be9
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-07-03 16:32:38 -0300

    styling the home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,6 @@
 import './styles.css';
 import { Header } from '../../components/Header';
-import { Footer } from '../../components/Footer'
+import { Footer } from '../../components/Footer';
 
 export function Home() {
   return (
@@ -11,9 +11,9 @@ export function Home() {
       <hr />
 
       <section className="conatainer">
-        <p>Aqui você vai ver meu filmes e séries favoritos.</p>
+        <p className="here">Aqui você vai ver meu filmes e séries favoritos.</p>
 
-        <p>As mais que me marcaram a minha vida e as melhores que eu vi na na minha vida</p>
+        <p className="scored">As mais que me marcaram a minha vida e as melhores que eu vi na na minha vida</p>
 
         <img src="https://64.media.tumblr.com/e5b9277b238c9ad00a0a33f231290bdd/c1b22cfab0db44b9-1d/s500x750/f34ddac7bb0817a56f96dc949e1276a84b520146.jpg" alt="" />
       </section>
