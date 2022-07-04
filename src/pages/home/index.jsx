b430be2033e887f90c8688bbbafd721bf2a3e9dc
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-07-03 21:53:00 -0300

    styling the 'button' and switching images

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -14,7 +14,7 @@ export function Home() {
 
         <p className="scored">As mais que me marcaram a minha vida e as melhores que eu vi na na minha vida</p>
 
-        <img src="https://64.media.tumblr.com/e5b9277b238c9ad00a0a33f231290bdd/c1b22cfab0db44b9-1d/s500x750/f34ddac7bb0817a56f96dc949e1276a84b520146.jpg" alt="" />
+        <img src="https://i.ytimg.com/vi/eJWE26KvE94/maxresdefault.jpg" alt="" />
       </section>
 
       <Footer />
